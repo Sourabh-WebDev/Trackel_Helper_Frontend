a88022a13560828042de56f9486f05ae6747c5ab
Sourabh Verma <vermaanurag5542@gmail.com>
2023-06-12 18:48:18 +0530

    Update FirstSection.js

--- src/Components/FirstSection.js
+++ src/Components/FirstSection.js
@@ -17,7 +17,7 @@ const FirstSection = () => {
                             <h1 className='FirstSectionHeadinng'>Home services, on demand. 👇</h1>
                             <InputGroup className="mb-3">
                                 <DropdownButton
-                                    title="Select your Location ▿"
+                                    title="Select your Location"
                                     id="input-group-dropdown-1"
                                     className='txtColour'
                                 >
