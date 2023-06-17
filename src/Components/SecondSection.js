36f05e0b1dfa5fea3d9e5815639c49eee519a32c
Sourabh Verma <vermaanurag5542@gmail.com>
2023-06-17 10:04:29 +0530

    added new category

--- src/Components/SecondSection.js
+++ src/Components/SecondSection.js
@@ -7,6 +7,8 @@ import Electrician from '../assets/img/ElectricianServices.png'
 import CarWashing from '../assets/img/CarService.png'
 import { BsFillCaretRightFill } from "react-icons/bs";
 import { useNavigate } from 'react-router';
+import { Col, Row } from 'reactstrap';
+import BackGrid from '../assets/img/White-Grid.png'
 
 const SecondSection = () => {
 
@@ -48,8 +50,22 @@ const SecondSection = () => {
                     <p>Get our services at minimum cost in a required time at the best deal possible with granted good work.</p>
                 </div>
 
-                <div className="bgSecondSectionImg">
-                    {PrimaryServices.map((item, index) => (
+                <div style={{ backgroundImage: `url(${BackGrid})` }} className="bgSecondSectionImg">
+                    <Row xs={1} md={1} lg={3} xl={5} className="g-4 pb-4">
+                        {PrimaryServices.map((item, index) => (
+                            <Col key={index}>
+                                <Card className='p-2 cardHover' style={{ height: '16rem' }}>
+                                    <div className="border">
+                                        <Card.Img height={150} src={item.image} />
+                                        <Card.Body>
+                                            <Card.Title>{item.title}</Card.Title>
+                                        </Card.Body>
+                                    </div>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                    {/* {PrimaryServices.map((item, index) => (
                         <Card style={{ cursor: 'pointer' }} onClick={() => navigate('/serviceName')} className=' cardHover' key={item.id}>
                             <Card.Img className='img-fluid' src={item.image} />
                             <Card.Body className='bgSecondSectionServiceCard'>
@@ -59,7 +75,7 @@ const SecondSection = () => {
                             </Card.Body>
                         </Card>
                     ))}
-                    <BsFillCaretRightFill onClick={() => navigate('/Our-All-Services')} style={{ cursor: 'pointer' }} size={50} />
+                    <BsFillCaretRightFill onClick={() => navigate('/Our-All-Services')} style={{ cursor: 'pointer' }} size={50} /> */}
                 </div>
             </div>
         </div>
