3367a5a018adbae8072ede0849a6d02461d681c6
Abhishek sharma <99015589+SharmaG-Dev@users.noreply.github.com>
2023-06-22 17:46:54 +0530

    updated roles and permission

--- src/AdminDashboards/Components/RolesAndPermission/AdminRoles.js
+++ src/AdminDashboards/Components/RolesAndPermission/AdminRoles.js
@@ -1,12 +1,61 @@
+import { Switch } from '@mui/material'
 import React, { Fragment } from 'react'
-import { Card } from 'reactstrap'
+import { Card, Col, Input, Row } from 'reactstrap'
 
 const AdminRoles = () => {
     return (
         <Fragment>
             <div className='p-3'>
-                <Card className='bg-glass'>
-                    This is the Admin Card
+                <Card className='bg-glass p-3 mt-2'>
+                    <h5 className='text-light p-3'>Roles & Permission (<span className='text-primary'>Admin</span>)</h5>
+                    <Row>
+                        <Col md={6}>
+                            <Card className='p-3 shadow-lg d-flex flex-column gap-3'>
+                                <div className='permissionWithSwitch d-flex align-items-center justify-content-between'>
+                                    <h6 className='Fw_600 text-blue'>Attendence</h6>
+                                    <Switch defaultChecked color="warning" />
+                                </div>
+
+                            </Card>
+                            <Card className='p-3 mt-3 shadow-lg d-flex flex-column gap-3'>
+                                <div className='permissionWithSwitch d-flex align-items-center justify-content-between'>
+                                    <h6 className='Fw_600 text-blue'>Expenses</h6>
+                                    <Switch defaultChecked color="warning" />
+                                </div>
+
+                            </Card>
+                            <Card className='p-3 mt-3 shadow-lg d-flex flex-column gap-3'>
+                                <div className='permissionWithSwitch d-flex align-items-center justify-content-between'>
+                                    <h6 className='Fw_600 text-blue'>Manage Customer</h6>
+                                    <Switch defaultChecked color="warning" />
+                                </div>
+
+                            </Card>
+                        </Col>
+                        <Col md={6}>
+                            <Card className='p-3 shadow-lg d-flex flex-column gap-3'>
+                                <div className='permissionWithSwitch d-flex align-items-center justify-content-between'>
+                                    <h6 className='Fw_600 text-blue'>Manage HR</h6>
+                                    <Switch defaultChecked color="warning" />
+                                </div>
+
+                            </Card>
+                            <Card className='p-3 mt-3 shadow-lg d-flex flex-column gap-3'>
+                                <div className='permissionWithSwitch d-flex align-items-center justify-content-between'>
+                                    <h6 className='Fw_600 text-blue'>Manage Master</h6>
+                                    <Switch defaultChecked color="warning" />
+                                </div>
+
+                            </Card>
+                            <Card className='p-3 mt-3 shadow-lg d-flex flex-column gap-3'>
+                                <div className='permissionWithSwitch d-flex align-items-center justify-content-between'>
+                                    <h6 className='Fw_600 text-blue'>Manage Website</h6>
+                                    <Switch defaultChecked color="warning" />
+                                </div>
+
+                            </Card>
+                        </Col>
+                    </Row>
                 </Card>
             </div>
         </Fragment>
