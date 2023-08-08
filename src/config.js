a363c8c561f3c2e9a775ea35fd6a22deab2759dd
SharmaG-Dev <99015589+SharmaG-Dev@users.noreply.github.com>
2023-08-08 18:05:14 +0530

    Updated APi

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const API_URL = "https://helper-i1ln.onrender.com"
-// export const API_URL = "http://ec2-54-149-157-5.us-west-2.compute.amazonaws.com"
-export const API_URL = "http://localhost:8080"
+export const API_URL = "http://ec2-54-149-157-5.us-west-2.compute.amazonaws.com"
+// export const API_URL = "http://localhost:8080"
 
 
 
