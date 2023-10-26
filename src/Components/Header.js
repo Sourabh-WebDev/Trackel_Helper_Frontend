c3fc0e36323ebf67ca9cb0abf5cea65d78d0c25b
AbhishekTsFullStack <145094267+AbhishekTsFullStack@users.noreply.github.com>
2023-10-26 18:13:52 +0530

    Updated with sms login in every

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,86 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Logo from '../assets/svg/we_logo.png';
-import Swal from 'sweetalert2';
-import { NavDropdown } from 'react-bootstrap';
-import { UseStateManager } from '../Context/StateManageContext';
-import { LoginModal, SingupModal } from './Modal';
-import { useSelector } from 'react-redux';
-import { Logout } from '@mui/icons-material';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { Link, useNavigate } from 'react-router-dom';
-import { Grid, Paper, Typography } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Logo from "../assets/svg/we_logo.png";
+import Swal from "sweetalert2";
+import { NavDropdown } from "react-bootstrap";
+import { UseStateManager } from "../Context/StateManageContext";
+import { LoginModal, SingupModal } from "./Modal";
+import { useSelector } from "react-redux";
+import { Logout } from "@mui/icons-material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Link, useNavigate } from "react-router-dom";
+import { Grid, Paper, Typography } from "@mui/material";
 import { BsFillTelephoneFill } from "react-icons/bs";
 
 function Header() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("customer"))
+  );
+  const LoginResult = useSelector((pre) => pre.GetLogInReducers);
+  const { LoginOpen, setLoginOpen } = UseStateManager();
 
-    const [currentUser, setCurrentUser] = useState(
-        JSON.parse(sessionStorage.getItem('user'))
-    );
-    const LoginResult = useSelector((pre) => pre.GetLogInReducers);
-    const { LoginOpen, setLoginOpen } = UseStateManager();
+  const GetLogOut = () => {
+    sessionStorage.removeItem("customer");
+    setCurrentUser(null);
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Logout Successfully",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    navigate("/");
+  };
 
-    const GetLogOut = () => {
-        sessionStorage.removeItem('user');
-        setCurrentUser(null);
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Logout Successfully',
-            showConfirmButton: false,
-            timer: 1500,
-        });
-        navigate('/')
-    };
+  useEffect(() => {
+    if (LoginResult.isSuccess === true) {
+      setLoginOpen(false);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Log In Successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setCurrentUser(JSON.parse(sessionStorage.getItem("customer")));
+    } else if (LoginResult.isSuccess === false) {
+      Swal.fire({
+        position: "bottom-end",
+        icon: "error",
+        title: "Ooops ! Log In failed",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  }, [LoginResult, setLoginOpen]);
 
-    useEffect(() => {
-        if (LoginResult.isSuccess === true) {
-            setLoginOpen(false);
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: 'Log In Successfully',
-                showConfirmButton: false,
-                timer: 1500,
-            });
-            setCurrentUser(JSON.parse(sessionStorage.getItem('user')));
-        } else if (LoginResult.isSuccess === false) {
-            Swal.fire({
-                position: 'bottom-end',
-                icon: 'error',
-                title: 'Ooops ! Log In failed',
-                showConfirmButton: false,
-                timer: 1500,
-            });
-        }
-    }, [LoginResult, setLoginOpen]);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+  return (
+    <Navbar className="bgColour" expand="lg">
+      <Container fluid>
+        <Navbar.Brand className="animate__animated animate__flipInY" href="/">
+          <img src={Logo} alt="MainLogo" />
+        </Navbar.Brand>
 
-    return (
-        <Navbar className='bgColour' expand='lg'>
-            <Container fluid>
-                <Navbar.Brand className='animate__animated animate__flipInY' href='/'>
-                    <img src={Logo} alt='MainLogo' />
-                </Navbar.Brand>
-
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                    <Nav
-                        // style={{ marginLeft: '200px' }}
-                        className='ml-auto my-2 my-lg-0 text-center'>
-
-                        {/* <Nav.Link className='txtColour' href='#action1'>Register As A Professional</Nav.Link> */}
-                        <Nav.Link className='txtColour' href='/'><b>HOME</b></Nav.Link>
-                        {/* <div className="mega-menu d-none d-md-block bgColour">
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav
+            // style={{ marginLeft: '200px' }}
+            className="ml-auto my-2 my-lg-0 text-center"
+          >
+            {/* <Nav.Link className='txtColour' href='#action1'>Register As A Professional</Nav.Link> */}
+            <Nav.Link className="txtColour" href="/">
+              <b>HOME</b>
+            </Nav.Link>
+            {/* <div className="mega-menu d-none d-md-block bgColour">
                             <Nav.Link className='txtColour' sx={{ pt: 1 }} onClick={toggleMenu}>
                                 <b>SERVICES</b>
                                 <ArrowDropDownIcon />
@@ -137,33 +138,61 @@ function Header() {
                                 </Paper>
                             )}
                         </div> */}
-                        <Nav.Link className='txtColour' href='/About-Us'><b>ABOUT US</b></Nav.Link>
-                        {/* <Nav.Link className='txtColour' href='/'><b>Services</b></Nav.Link> */}
-                        {/* <Nav.Link className='txtColour' href='/Contact-Us'><b>Help</b></Nav.Link> */}
-                        {currentUser !== null ? <>
-                            <Nav.Link className='txtColour' href={`/YourProfile/${currentUser._id}`} >{currentUser.name}</Nav.Link>
-                            <Nav.Link className='txtColour' ><Logout className='cursor-p' onClick={() => GetLogOut()} /></Nav.Link>
-                        </> :
-                            <>
-                                <NavDropdown className='font-weight-bold txtColour  ' title='LOGIN' id='nav-dropdown'>
-                                    <NavDropdown.Item onClick={() => setLoginOpen(!LoginOpen)} eventKey='4.1'>Customer Login</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={() => navigate('/admin')} eventKey='4.2'>Office Login</NavDropdown.Item>
-                                </NavDropdown>
-                            </>
-                        }
-                    </Nav>
-                    {/* <Nav.Link className='txtColour' href='/Contact-Us'><b>HELP</b></Nav.Link> */}
-                </Navbar.Collapse>
-                <div style={{ border: '1px solid #4a59e4' }} className='bgColour d-flex align-items-center rounded-pill px-2 m-1 d-none d-md-block'>
-                    <BsFillTelephoneFill color='#4a59e4' />
-                    <Typography variant='caption'>&nbsp;0522-4300589</Typography>
-                </div>
-            </Container>
-            <LoginModal />
-            <SingupModal />
-        </Navbar >
-    );
+            <Nav.Link className="txtColour" href="/About-Us">
+              <b>ABOUT US</b>
+            </Nav.Link>
+            {/* <Nav.Link className='txtColour' href='/'><b>Services</b></Nav.Link> */}
+            {/* <Nav.Link className='txtColour' href='/Contact-Us'><b>Help</b></Nav.Link> */}
+            {currentUser !== null ? (
+              <>
+                <Nav.Link
+                  className="txtColour"
+                  href={`/YourProfile/${currentUser._id}`}
+                >
+                  {currentUser.name}
+                </Nav.Link>
+                <Nav.Link className="txtColour">
+                  <Logout className="cursor-p" onClick={() => GetLogOut()} />
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <NavDropdown
+                  className="font-weight-bold txtColour  "
+                  title="LOGIN"
+                  id="nav-dropdown"
+                >
+                  <NavDropdown.Item
+                    onClick={() => setLoginOpen(!LoginOpen)}
+                    eventKey="4.1"
+                  >
+                    Customer Login
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    onClick={() => navigate("/admin")}
+                    eventKey="4.2"
+                  >
+                    Office Login
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            )}
+          </Nav>
+          {/* <Nav.Link className='txtColour' href='/Contact-Us'><b>HELP</b></Nav.Link> */}
+        </Navbar.Collapse>
+        <div
+          style={{ border: "1px solid #4a59e4" }}
+          className="bgColour d-flex align-items-center rounded-pill px-2 m-1 d-none d-md-block"
+        >
+          <BsFillTelephoneFill color="#4a59e4" />
+          <Typography variant="caption">&nbsp;0522-4300589</Typography>
+        </div>
+      </Container>
+      <LoginModal />
+      <SingupModal />
+    </Navbar>
+  );
 }
 
 export default Header;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // export const API_URL = "https://helper-i1ln.onrender.com"
 // export const API_URL = "http://ec2-43-204-74-189.ap-south-1.compute.amazonaws.com"
 export const API_URL = "https://server.ssquickhelper.com";
-// export const API_URL = "http://localhost:5000"
+// export const API_URL = "http://localhost:8080";
 
 export const employeeList = ["backoffice", "admin", "supervisor"];
 
